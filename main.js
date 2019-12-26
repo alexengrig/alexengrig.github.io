1e346b2359f56758218534655423dc09e6d97c3b
Grig Alex <36041152+alexengrig@users.noreply.github.com>
2019-12-26 17:17:01 +0300

    Update main.js

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ fetch('https://github-contributions-api.now.sh/v1/alexengrig')
   .then(contributions => {
     const now = new Date().toISOString().substring(0, 10);
     const contribution = contributions.find(contribution => contribution.date === now);
+    console.debug('contribution', contribution);
     return contribution.color;
   })
   .then(color => document.body.style.background = color)
